fa043cfd6fda68aabd97e4ad4073fbfd655ec8b0
Ashu23Queen <165048198+Ashu23Queen@users.noreply.github.com>
2024-06-22 15:52:18 +0000

    write component in pages added

---
+++ src/pages/write/Write.jsx
@@ -0,0 +1,22 @@
+import "./write.css"
+
+export default function Write() {
+    return(
+        <div className="write">
+            <form className="writeForm">
+                <div className="writeFormGroup">
+                    <label htmlFor="fileInput">
+                        <i class="fas fa-plus"></i>
+                    </label>
+                    <input type="file" id="fileInput" style={{display:"none"}} />
+                    {/* <input type="file" id="fileInput" /> */}
+                    <input type="text" placeholder="Title" className="writeInput" autoFocus="{true}" />
+                </div>
+                <div className="writeFormGroup">
+                    <textarea name="" id="" placeholder="Tell your story..." type="text" className="writeInput writeText"></textarea>
+                </div>
+                <button className="writeSubmit">Publish</button>
+            </form>
+        </div>
+    )    
+}
