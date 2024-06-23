bd3ad96a118eb97a25358f916e99ba719af30f29
Ashu23Queen <165048198+Ashu23Queen@users.noreply.github.com>
2024-06-23 08:56:10 +0000

    settings component created in pages folder and also added form in it....

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import Home from "./pages/home/Home";
 import TopBar from "./components/topbar/TopBar";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
-
+import Settings from "./pages/settings/Settings";
+ 
  
 function App() {
   return (
     <>
     <TopBar/>
-    <Write/>
+    <Settings/>
     </>
   );
 }
