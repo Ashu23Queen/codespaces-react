ac7a6b857ca06a217c7149a780cb19b6e070e5cd
Ashu23Queen <165048198+Ashu23Queen@users.noreply.github.com>
2024-06-24 07:33:06 +0000

    login and regiter components created along with css... :tada:

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import TopBar from "./components/topbar/TopBar";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
 import Settings from "./pages/settings/Settings";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
  
  
 function App() {
   return (
     <>
     <TopBar/>
-    <Settings/>
+    <Register/>
     </>
   );
 }
