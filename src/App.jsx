c3a93d9da2baab520721bf5d412b1b22c8ab02c1
Ashu23Queen <165048198+Ashu23Queen@users.noreply.github.com>
2024-07-06 09:34:06 +0000

    working on menu linking part

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,16 @@ import Write from "./pages/write/Write";
 import Settings from "./pages/settings/Settings";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
- 
- 
+//import { BrowserRouter as Router ,Routes ,Route ,Link} from 'react-router-dom';
+//import { BrowserRouter as Router,Routes,Route ,Link} from 'react-router-dom';
+
+
+
 function App() {
   return (
     <>
     <TopBar/>
-    <Login/> 
+      <Register/>    
     </>
   );
 }
